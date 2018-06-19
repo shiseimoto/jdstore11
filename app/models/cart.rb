998a7b297b92600bb3d9baf7cf98f9588f98837a
shichengyuan <shiseimoto@gmail.com>
2018-06-19 09:42:28 +0800

    adsd

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
-  has_many :cart_items
-  has_many :products, through: :cart_items, source: :product
+    has_many :cart_items
+    has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
     ci = cart_items.build
@@ -8,5 +8,4 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
-  
 end
